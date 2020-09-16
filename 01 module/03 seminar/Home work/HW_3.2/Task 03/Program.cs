e1de9f0fe80acf585b50c7d4d756f5193608291b
Никита Бородин <nsborodin@edu.hse.ru>
2020-09-17 02:04:24 +0300

    Загружаю ДЗ 3

---
+++ 01 module/03 seminar/Home work/HW_3.2/Task 03/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Test_03
+{
+    class Program
+    {
+        static double MyMethod(double X)
+        {
+            if (X > 0.5)
+                return Math.Sin(Math.PI * (X - 1) / 2);
+            else
+                return Math.Sin(Math.PI / 2);
+        }
+        static void Main(string[] args)
+        {
+            if(!double.TryParse(Console.ReadLine(), out double X))
+            {
+                Console.WriteLine("Error");
+                return;
+            }
+
+            Console.WriteLine("G = {0:F3}", MyMethod(X));
+        }
+    }
+}
---
+++ 01 module/03 seminar/Home work/HW_3.2/Task 04/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Num = new int[3];
+
+
+            for (int i = 0; i < Num.Length; i++)
+            {
+                if (!int.TryParse(Console.ReadLine(), out Num[i]))
+                {
+                    Console.WriteLine("Error");
+                    return;
+                }
+            }
+
+            int result = Num[0] % 100 < Num[1] % 100 ? (Num[0] % 100 < Num[2] % 100 ? Num[0] : Num[2]) :
+                (Num[1] % 100 < Num[2] % 100 ? Num[1] : Num[2]);
+
+            Console.WriteLine(result);
+        }
+    }
+}
